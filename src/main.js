a17431093f5c34278f1b04f97adc3cd7f9aeb5c1
pbiryuk <biryuk.p.biryuk@gmail.com>
2025-05-23 16:34:22 +0300

    test1

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import './css/styles.css';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.warning({ message: 'Please enter a search term!' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    if (data.hits.length === 0) {
+      iziToast.info({
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (err) {
+    iziToast.error({ message: 'Something went wrong. Try again later.' });
+  } finally {
+    hideLoader();
+  }
+});
